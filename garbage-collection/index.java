b95655b603f4fb78a9796a2eeb65a0795d4e4698
David William <davidwilliam35302@gmail.com>
2024-09-24 21:04:50 +0530

    Garbage collection

--- garbage-collection/index.java
+++ garbage-collection/index.java
@@ -1,22 +1,10 @@
 public class index {
   public static void main(String[] args) {
-    Employee e1 = new Employee("John",23);
-    Employee e2 = new Employee("Doye", 45);
-    e1.display();
-    e2.display();
-    e1.nextId();
-    e2.nextId();
-    {
-      Employee i1 = new Employee("Intern-1",19);
-      Employee i2 = new Employee("Intern-2",18);
-      i1.nextId();
-      i2.nextId(); 
-      // say interns i1 and i2 were fired
-      i1 = i2 = null;
-      // request java to do garbage collection
-      System.gc();
-      System.runFinalization(); // because i overloaded the finalize() method
-    }
-    e1.nextId();
+    String num1 = "100";
+    String num2 = "200";
+    System.out.println(num1 + num2);
+    // say num2 isnt required
+    num2 = null;
+    System.gc();
   }
 }
