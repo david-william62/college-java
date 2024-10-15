d26e2d3cd29203d7fb7a181db5745beb463b68e8
david-william62 <davidwilliam35302@gmail.com>
2024-10-15 12:36:39 +0530

    0

--- calculator/calculator.java
+++ calculator/calculator.java
@@ -1,44 +1,81 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
-class NumButton {
- public JButton createBtn(String name, int bound) {
-  JButton b = new JButton(name);
-  b.setBounds(100,bound,50,40);
-  return b;
- }
-}
-
 class calc {
+ private JFrame f;
+ private JTextField display;
+ private double number1, number2;
+ private char operation;
+
  public calc() {
-  NumButton num = new NumButton();
-  JFrame f = new JFrame();
-  JButton n1 = num.createBtn("1",50);
-  JButton n2 = num.createBtn("2",100);
-  JButton n3 = num.createBtn("3",150);
-  JButton n4 = num.createBtn("4",200);
-  JButton n5 = num.createBtn("5",250);
-  JButton n6 = num.createBtn("6",300);
-  JButton n7 = num.createBtn("7",350);
-  JButton n8 = num.createBtn("8",400);
-  JButton n9 = num.createBtn("9",450);
-  JButton n0 = num.createBtn("0",500);
-  f.add(n1);
-  f.add(n2);
-  f.add(n3);
-  f.add(n4);
-  f.add(n5);
-  f.add(n6);
-  f.add(n7);
-  f.add(n8);
-  f.add(n9);
-  f.add(n0);
-  f.setSize(1000,1000);
-  f.setLayout(null);
+  f = new JFrame();
+  f.setSize(300, 300);
+  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  f.setLayout(new BorderLayout());
+
+  display = new JTextField();
+  display.setEditable(false);
+
+  JPanel btnPanel = new JPanel(new GridLayout(4, 4));
+
+  String[] labels = {"1", "2", "3", "+",
+                "4", "5", "6", "-",
+                "7", "8", "9", "%",
+                "0", "*", "/", "="};
+
+  for (String label : labels) {
+   JButton btn = new JButton(label);
+   btn.addActionListener(new ButtonListener());
+   btnPanel.add(btn);
+  }
+
+  f.add(display, BorderLayout.NORTH);
+  f.add(btnPanel, BorderLayout.CENTER);
   f.setVisible(true);
  }
 
- public static void main(String[] args) {
-  new calc();
+ private class ButtonListener implements ActionListener {
+  @Override
+  public void actionPerformed(ActionEvent e) {
+   String command = e.getActionCommand();
+
+   switch (command) {
+    case "0": case "1": case "2": case "3": case "4":
+    case "5": case "6": case "7": case "8": case "9":
+     display.setText(display.getText() + command); 
+     break;
+    case "+": case "-": case "*": case "/": case "%":
+     number1 = Double.parseDouble(display.getText());
+     operation = command.charAt(0);
+     display.setText(""); 
+     break;
+    case "=":
+     number2 = Double.parseDouble(display.getText());
+     double result = calculate(number1, number2, operation);
+     display.setText(String.valueOf(result)); 
+     break;
+   }
+  }
+
+  private double calculate(double num1, double num2, char op) {
+   switch (op) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    case '/': 
+     if(num2 != 0) return num1 / num2;
+     else {
+      JOptionPane.showMessageDialog(null, "Error! Division by zero is not allowed.");
+      return 0;
+     }
+     case '%': return num1 % num2;
+     default: return 0;
+    }
+   }
+  }
+
+  public static void main(String[] args) {
+   new calc();
+  }
  }
-}
